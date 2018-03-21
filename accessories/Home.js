11c739cb9a6252fb0a3d57b90c55832f4e130d69
Seyd <34152761+SeydX@users.noreply.github.com>
2018-03-21 03:12:48 +0100

    Update Home.js

--- accessories/Home.js
+++ accessories/Home.js
@@ -161,7 +161,7 @@ class HOMEAPP {
                         setTimeout(function() {
                             self.setOnCount += 1;
                             self.HomeSwitch.getCharacteristic(Characteristic.On).setValue(self.state);
-                        }, 3000￼)
+                        }, 3000)
                         callback(null, self.state)
                     } else {
                         self.state = false;
